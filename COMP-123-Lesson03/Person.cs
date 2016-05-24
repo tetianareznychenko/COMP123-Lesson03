d8e8833635bc49f5577a3f7a664bf0f6db3c4392
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-05-24 12:55:00 -0400

    Added person class

---
+++ COMP-123-Lesson03/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_Lesson03
+{
+    class Person
+    {
+    }
+}
